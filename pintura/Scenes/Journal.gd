bc975407820436be6795c7337e2e91fbce87a119
euphori <56996213+euphori@users.noreply.github.com>
2023-04-06 12:41:04 +0800

    journal page turner

--- pintura/Scenes/Journal.gd
+++ pintura/Scenes/Journal.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+var page = 0
+onready var max_page = $PageContainer.get_child_count() - 1
+onready var page_slots = $PageContainer.get_children()
+
+
+func _ready():
+	page_slots[0].visible = true
 
 
 func _input(event):
@@ -8,3 +15,28 @@ func _input(event):
 			visible = true
 		else:
 			visible = false
+
+
+
+func update_page():
+	
+	page_slots[page].visible = true
+	for i in max_page + 1:
+		if i != page:
+			page_slots[i].visible = false
+	
+	
+
+
+func _on_ButtonLeft_pressed():
+	if page > 0:
+		page -= 1
+		print(page)
+		update_page()
+
+
+func _on_ButtonRight_pressed():
+	if page < max_page:
+		page += 1
+		print(page)
+		update_page()
