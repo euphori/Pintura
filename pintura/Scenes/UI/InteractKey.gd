22cfdc1f4e1b201b54c312cb318f50c92d4f0cb5
euphori <56996213+euphori@users.noreply.github.com>
2023-04-11 01:47:24 +0800

    minor fix

--- pintura/Scenes/UI/InteractKey.gd
+++ pintura/Scenes/UI/InteractKey.gd
@@ -2,20 +2,30 @@ extends Sprite
 
 
 
-export (String, "Open", "Talk", "Toggle") var text
+export (String, "Open", "Talk", "Toggle", "Grab") var text
+
 
 func _ready():
+
 	$Label.text = text
 	$AnimationPlayer.play("bob")
 	
 
 
 func _on_Area2D_area_entered(area):
-	visible = true
-	get_parent().player_near = true
+	if get_parent().get_name() == "Door":
+		if get_parent().switch_on == false and get_parent().need_switch == true:
+			visible = false
+		else:
+			visible = true
+	else:
+		visible = true
+	if get_parent().get("player_near") != null:
+		get_parent().player_near = true
 
 
 func _on_Area2D_area_exited(area):
 	visible = false
-	get_parent().player_near = false
+	if get_parent().get("player_near") != null:
+		get_parent().player_near = false
 
