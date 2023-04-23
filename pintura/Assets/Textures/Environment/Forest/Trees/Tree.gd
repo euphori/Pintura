5703ab193df75be6aeeaaed89001a91006d9a844
euphori <56996213+euphori@users.noreply.github.com>
2023-04-24 01:51:32 +0800

    updated world map and bug fix

--- pintura/Assets/Textures/Environment/Forest/Trees/Tree.gd
+++ pintura/Assets/Textures/Environment/Forest/Trees/Tree.gd
@@ -1,14 +1,14 @@
 extends StaticBody2D
 
-var green = preload("res://Assets/Textures/Environment/Forest/Trees/green_birch.tres")
-var yellow = preload("res://Assets/Textures/Environment/Forest/Trees/yellow_birch.tres")
-var rng = RandomNumberGenerator.new()
-func _ready():
-	rng.randomize()
-	var color = rng.randi_range(0,2)
-	if color == 1:
-		$Birch.set_texture(green)
-	else:
-		$Birch.set_texture(yellow)
+#var green = preload("res://Assets/Textures/Environment/Forest/Trees/green_birch.tres")
+#var yellow = preload("res://Assets/Textures/Environment/Forest/Trees/yellow_birch.tres")
+#var rng = RandomNumberGenerator.new()
+#func _ready():
+	#rng.randomize()
+	#var color = rng.randi_range(0,2)
+	#if color == 1:
+		#$Birch.set_texture(green)
+	#else:
+		#$Birch.set_texture(yellow)
 
 
--- pintura/Scenes/Maps/WorldMap.gd
+++ pintura/Scenes/Maps/WorldMap.gd
@@ -6,4 +6,4 @@ export var hide_trees = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !hide_trees:
-		$Trees.visible = true
+		$YSort/Trees.visible = true
