1eb6818881805f382850a848b9905f6f403af0fa
mr.robt <sirpercival909@gmail.com>
2023-04-24 23:32:54 +0800

    BGM added

--- pintura/Scenes/MusicController.gd
+++ pintura/Scenes/MusicController.gd
@@ -3,6 +3,11 @@ onready var player = get_parent()
 var bg_museum = load("res://Music/Music Of Something.mp3")
 var bg_tunnel = load("res://Music/An Ancient King.mp3")
 var bg_world_start = load("res://Music/An Ugly Heart But It does Beats.mp3")
+var bgm_chased = load("res://Music/Chasing.mp3")
+var bgm_calm = load("res://Music/calm.mp3")
+var bgm_festive = load("res://Music/Festive.mp3")
+
+var bgm_seashore = load("res://Music/Sound Effects/Calm Seashore.mp3")
 var dialogue_sound = load("res://Music/Sound Effects/DialogueSound.mp3")
 var MenuSound = load("res://Music/Sound Effects/MenuSound.wav")
 
