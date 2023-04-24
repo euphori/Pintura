18261aa44de70363e6198b2489361ad9cfd7eb24
mr.robt <sirpercival909@gmail.com>
2023-04-24 22:12:19 +0800

    Dialogue Sound

--- pintura/Scenes/MusicController.gd
+++ pintura/Scenes/MusicController.gd
@@ -4,6 +4,8 @@ extends Node
 var bg_Music = load("res://Music/Music Of Something.mp3")
 var bg_Tmusic = load("res://Music/An Ancient King.mp3")
 var bg_Wmusic = load("res://Music/In Dreamlandbackground music.mp3")
+var dialogue_Sound = load("res://Music/Sound Effects/DialogueSound.mp3")
+var menu_Sound = load("res://Music/Sound Effects/MenuSound.wav")
 
 func _ready():
 	pass
@@ -22,5 +24,12 @@ func play_Wmusic():
 	
 	$Music.stream = bg_Wmusic
 	$Music.play()
-
+	
+func play_dialogue_Sound():
+	$Music.stream = dialogue_Sound
+	$Music.play()
+	
+func play_menu_Sound():
+	$Music.stream = menu_Sound
+	$Music.play()
 
