b31c4dfd016030de1e9608c367fa9734d87d7bf8
mr.robt <sirpercival909@gmail.com>
2023-05-09 19:24:37 +0800

    pusg

--- pintura/Scripts/Player.gd
+++ pintura/Scripts/Player.gd
@@ -83,7 +83,10 @@ func _input(event):
 	if event.is_action_pressed("throw"):
 		throw()
 		
-
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == BUTTON_RIGHT:
+			self.global_position = get_global_mouse_position()
+	
 
 func throw():
 	var garlic_path = garlic_scene
